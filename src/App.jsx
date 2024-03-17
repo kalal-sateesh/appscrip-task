67991cb44112ab4f3a178655109eee733cd87a00
Sateesh Kumar Goud <kalalsateesh893@gmail.com>
2024-03-17 19:02:57 +0530

    main tag added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ function App() {
   return (
     <div className="App">
       <AppHeader />
-      <HomePage />
+      <main>
+        <HomePage />
+      </main>
       <AppFooter />
     </div>
   );
